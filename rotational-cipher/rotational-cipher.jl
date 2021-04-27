0f253cf0817b1a64fa16e9b7a443d9ce899df457
Stuart Clothier <stuartclothier@gmail.com>
2021-04-27 14:55:31 +0100

    commit on 'master'

--- rotational-cipher/rotational-cipher.jl
+++ rotational-cipher/rotational-cipher.jl
@@ -1,6 +1,9 @@
-function rotate(ROT,string)
+function rotate(ROT, string)
+    
     codebase = [i for i in 1:126]
-    [(codebase[i],codebase[j]) = (Int('A')+(i-Int('A')+ROT)%26, Int('a')+(j-Int('a')+ROT)%26) for i in 65:90 for j in 97:122];
+    [(codebase[i + Int('A')], codebase[i + Int('a')]) = 
+    (Int('A') + (i + ROT) % 26, Int('a') + (i + ROT) % 26) 
+    for i in 0:25];
 
     Coded_array = [Char(codebase[Int(each)]) for each in string]
     if typeof(string) == String
@@ -12,6 +15,7 @@ end
 
 for i = 0:26
     @eval macro $(Symbol("R$(i)_str"))(s)
-        rotate($i,s)
+        rotate($i, s)
     end
 end
+
