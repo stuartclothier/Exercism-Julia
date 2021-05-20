65bb2eb6d5ef446413e6e1f601bc68a223961f12
Stuart Clothier <stuartclothier@gmail.com>
2021-05-20 15:16:02 +0100

    difference of squares expanded

--- difference-of-squares/difference-of-squares.jl
+++ difference-of-squares/difference-of-squares.jl
@@ -1,6 +1,11 @@
 "Square the sum of the first `n` positive integers"
 function square_of_sum(n::Int)
-    div(n * (n + 1), 2)^2
+    ## Multiple different methods are shown below for evaluating 
+    ## the square of the sum.
+    
+    (n^2+2n^3+n^4)÷4 
+    # evalpoly(n, (0,0,1,2,1)) ÷ 4 
+    # (n * (n + 1) ÷ 2)^2
 end
 
 "Sum the squares of the first `n` positive integers"
